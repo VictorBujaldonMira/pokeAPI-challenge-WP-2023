604bcb7f00ff5ca4f94873e6abf2fa5ab6e4530a
Víctor Bujaldón <victor@acceseo.com>
2023-10-28 22:12:27 +0200

    feat/Add class and enqueue js pokemon filter

--- wp-content/plugins/pokemon-manager/pokemon-manager.php
+++ wp-content/plugins/pokemon-manager/pokemon-manager.php
@@ -15,6 +15,7 @@ if (!defined('ABSPATH')) {
 require_once plugin_dir_path(__FILE__) . 'includes/class-pokemon-post-type.php';
 require_once plugin_dir_path(__FILE__) . 'includes/class-pokemon-meta-boxes.php';
 require_once plugin_dir_path(__FILE__) . 'includes/class-pokemon-ajax.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-pokemon-shortcodes.php';
 
 // On plugin load, initialize classes
 function init_pokemon_manager() {
@@ -56,6 +57,8 @@ function enqueue_styles_and_scripts(){
     wp_enqueue_script('pokemon-ajax', plugin_dir_url(__FILE__) . 'assets/js/dist/pokemon-ajax.js', array(), '1.0', true);
 
     wp_add_inline_script('pokemon-ajax', 'window.ajaxurl = "' . admin_url('admin-ajax.php') . '";', 'before');
+
+    wp_enqueue_script('pokemon-filter', plugin_dir_url(__FILE__) . 'assets/js/dist/pokemon-filter.js', array(), '1.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'enqueue_styles_and_scripts');
