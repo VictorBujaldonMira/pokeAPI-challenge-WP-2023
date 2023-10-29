d838bc3c930320c6cff32257b5d36789140ab19b
Víctor Bujaldón <victor@acceseo.com>
2023-10-29 02:03:52 +0200

    fix/Add comments and reorder code

--- wp-content/plugins/pokemon-manager/includes/class-pokemon-shortcodes.php
+++ wp-content/plugins/pokemon-manager/includes/class-pokemon-shortcodes.php
@@ -7,8 +7,10 @@ class Pokemon_Shortcodes {
     }
 
     public function generate_pokemon_grid($atts) {
+        // Determine the current page number.
         $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
+        // Setup the query arguments to fetch Pokemon posts.
         $query_args = array(
             'post_type' => 'pokemon',
             'posts_per_page' => 6,
@@ -16,8 +18,13 @@ class Pokemon_Shortcodes {
         );
 
         $query = new WP_Query($query_args);
-        $output = '<div id="pokemon__grid"><div id="pokemon__type-filter"></div><button class="reset-filter btn btn-outline-primary">Reset filter</button><div class="pokemon__grid-list mt-4">';
+        // Start building the output for the Pokemon grid.
+        $output = '<div id="pokemon__grid">';
+        $output .= '<div id="pokemon__type-filter"></div>';
+        $output .= '<button class="reset-filter btn btn-outline-primary">Reset filter</button>';
+        $output .= '<div class="pokemon__grid-list mt-4">';
 
+        // Loop through each Pokemon post and build its HTML.
         while ($query->have_posts()) {
             $query->the_post();
             $pokemon_image = get_the_post_thumbnail_url();
@@ -31,9 +38,9 @@ class Pokemon_Shortcodes {
         $output .= '</div>'; // Ending #pokemon__grid
         $output .= '</div>'; // Ending .pokemon__grid-list
 
+        // Add pagination links.
         $output .= '<div class="pokemon__pagination mt-4 mb-5">';
-
-        $big = 999999999;
+        $big = 999999999; // Arbitrary big number to replace for real paged value.
         $output .= paginate_links(array(
             'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
             'format' => '?paged=%#%',
